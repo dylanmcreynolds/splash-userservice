7331e19fe38087684ced5eff1b3355f1a006a171
Dylan McReynolds <dmcreynolds@lbl.gov>
2021-07-15 20:01:31 +0000

    remove stub test

--- alshub/tests/test_service.py
+++ alshub/tests/test_service.py
@@ -1,8 +1,8 @@
-import pytest
+# import pytest
 
-from alshub.service import get_user
-from alshub.models import User, AccessGroup
+# from alshub.service import get_user
+# from alshub.models import User, AccessGroup
 
-@pytest.mark.asyncio
-def test_user():
-    async 
+# @pytest.mark.asyncio
+# def test_user():
+#     async 
